9ff4747e8e4e007228108c90c4a500b91833d3c7
arushi <arushiraizada2008@gmail.com>
2023-07-14 10:38:46 -0400

    Initial Commit

---
+++ sharingOnGitHub/sharingOnGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharingOnGitHubApp.swift
+//  sharingOnGitHub
+//
+//  Created by Arushi Raizada on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct sharingOnGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
